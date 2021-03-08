6bf0ab4c2556c635d4a071d7742e4f1004c7ba6c
Mark Potts <mpotts@redlineperf.com>
2021-03-08 13:27:24 -0600

    update for test intel_2020_intelmpi_g on orion [ci skip]

---
+++ release/8.1.0/orion/intel/2020/g/intelmpi/2020.2/out/build-intel_2020_intelmpi_g.bat
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#SBATCH --account=da-cpu
+#SBATCH --partition=orion
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --time=1:00:00
+#SBATCH --exclusive
+#SBATCH --output build-intel_2020_intelmpi_g.bat_%j.o
+export JOBID=$SLURM_JOBID
+set -x
+module load intel/2020.2 impi/2020.2 netcdf/4.7.4
+module list >& module-build.log
+export ESMF_NETCDF=nc-config
+export ESMF_DIR=/work/noaa/da/mpotts/sandbox/intel_2020_intelmpi_g
+export ESMF_COMPILER=intel
+export ESMF_COMM=intelmpi
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 40 clean 2>&1|tee clean_$JOBID.log 
+make -j 40 2>&1|tee build_$JOBID.log
+
