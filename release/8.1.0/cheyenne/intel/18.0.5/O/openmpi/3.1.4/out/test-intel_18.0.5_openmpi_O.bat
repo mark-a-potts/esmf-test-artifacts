97bb16727500872569ea03e4d1838b56ff230e53
Mark Potts <mark.potts@noaa.gov>
2021-03-08 14:19:16 -0700

    update for test intel_18.0.5_openmpi_O on cheyenne [ci skip]

---
+++ release/8.1.0/cheyenne/intel/18.0.5/O/openmpi/3.1.4/out/test-intel_18.0.5_openmpi_O.bat
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+#PBS -N test-intel_18.0.5_openmpi_O.bat
+#PBS -j oe
+#PBS -q regular
+#PBS -A p48503002
+#PBS -l select=1:ncpus=36:mpiprocs=36
+#PBS -l walltime=1:00:00
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /glade/scratch/mpotts/intel_18.0.5_openmpi_O
+set -x
+module load intel/18.0.5 openmpi/3.1.4 netcdf/4.6.3
+module list >& module-test.log
+
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/glade/scratch/mpotts/intel_18.0.5_openmpi_O
+export ESMF_COMPILER=intel
+export ESMF_COMM=openmpi
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make install 2>&1|tee install_$JOBID.log 
+make all_tests 2>&1|tee test_$JOBID.log 
+
