f59faec5884c679b70c639115801c04f7bb542dc
Mark Potts <mpotts@redlineperf.com>
2021-03-08 13:26:48 -0600

    update for test gfortran_8.3.0_openmpi_O on orion [ci skip]

---
+++ release/8.1.0/orion/gfortran/8.3.0/O/openmpi/4.0.2/out/test-gfortran_8.3.0_openmpi_O.bat
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+#SBATCH --account=da-cpu
+#SBATCH --partition=orion
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --time=1:00:00
+#SBATCH --exclusive
+#SBATCH --output test-gfortran_8.3.0_openmpi_O.bat_%j.o
+export JOBID=$SLURM_JOBID
+set -x
+module load gcc/8.3.0 openmpi/4.0.2 netcdf/4.7.2
+module list >& module-test.log
+export ESMF_NETCDF=nc-config
+export LD_PRELOAD=/apps/gcc-8/gcc-8.3.0/lib64/libstdc++.so
+export ESMF_DIR=/work/noaa/da/mpotts/sandbox/gfortran_8.3.0_openmpi_O
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=openmpi
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make install 2>&1|tee install_$JOBID.log 
+make all_tests 2>&1|tee test_$JOBID.log 
+
