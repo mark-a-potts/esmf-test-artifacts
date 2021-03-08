0e10121e5d237369482e4444de4043e7d06e6f15
Mark Potts <mark.potts@noaa.gov>
2021-03-08 14:21:17 -0700

    update for test intel_18.0.5_mpt_g on cheyenne [ci skip]

---
+++ release/8.1.0/cheyenne/intel/18.0.5/g/mpt/2.19/out/test-intel_18.0.5_mpt_g.bat
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+#PBS -N test-intel_18.0.5_mpt_g.bat
+#PBS -j oe
+#PBS -q regular
+#PBS -A p48503002
+#PBS -l select=1:ncpus=36:mpiprocs=36
+#PBS -l walltime=1:00:00
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /glade/scratch/mpotts/intel_18.0.5_mpt_g
+set -x
+module load intel/18.0.5 mpt/2.19 netcdf/4.6.3
+module list >& module-test.log
+
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/glade/scratch/mpotts/intel_18.0.5_mpt_g
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpt
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make install 2>&1|tee install_$JOBID.log 
+make all_tests 2>&1|tee test_$JOBID.log 
+
