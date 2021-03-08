9b8645e124fdfbc3fb3dd4989062e75c4ce81bfb
Mark A Potts <Mark.Potts@noaa.gov>
2021-03-08 15:56:24 -0500

    update for test gfortran_8.3.0_mpi_O on gaea [ci skip]

---
+++ release/8.1.0/gaea/gfortran/8.3.0/g/mpiuni/out/test-gfortran_8.3.0_mpiuni_g.bat
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+#SBATCH --account=nggps_emc
+#SBATCH --cluster=c4
+#SBATCH --qos=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=2:00:00
+#SBATCH --exclusive
+#SBATCH --output test-gfortran_8.3.0_mpiuni_g.bat_%j.o
+export JOBID=$SLURM_JOBID
+set -x
+
+module unload PrgEnv-intel
+
+module load PrgEnv-gnu
+module load gcc/8.3.0  cray-netcdf/4.6.3.2
+module list >& module-test.log
+
+export ESMF_NETCDF=nc-config
+
+export ESMF_NETCDF_LIBS="-lnetcdff -lnetcdf"
+export ESMF_DIR=/lustre/f2/dev/ncep/Mark.Potts/gfortran_8.3.0_mpiuni_g
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make install 2>&1|tee install_$JOBID.log 
+make all_tests 2>&1|tee test_$JOBID.log 
+
