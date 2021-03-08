a0ae3751c90fd2d59676feb316edd3df953c0667
Mark Allen Potts <mpotts@koehr01.ib0.icexa.navydsrc.hpc.mil>
2021-03-08 23:55:36 +0000

    update for test gfortran_7.3.0_mpt_O on koehr [ci skip]

---
+++ release/8.1.0/koehr/gfortran/7.3.0/O/mpt/2.20/out/build-gfortran_7.3.0_mpt_O.bat
@@ -0,0 +1,36 @@
+#!/bin/bash -l
+#PBS -N build-gfortran_7.3.0_mpt_O.bat
+#PBS -j oe
+#PBS -q standard
+#PBS -A NRLMR03795024
+#PBS -l select=1:ncpus=48:mpiprocs=48
+#PBS -l walltime=1:00:00
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /p/work1/mpotts/gfortran_7.3.0_mpt_O
+set -x
+
+module unload compiler/intel mpt
+module load gcc/7.3.0 mpt/2.20 netcdf-c/gnu/4.3.3.1
+module load netcdf-c/gnu/4.4.2 
+module list
+module list >& module-build.log
+
+export ESMF_NETCDF=nc-config
+
+export ESMF_NETCDF_LIBPATH="/app/COST/netcdf-fortran/4.4.2/gnu/lib /app/COST/netcdf-c/4.3.3.1/gnu//lib /app/COST/hdf5/1.8.15/gnu//lib"
+export ESMF_NETCDFF_LIBS="-lnetcdf -lnetcdf -lhdf5 -lhdf5_hl -lz"
+export ESMF_NETCDFF_LIBS="-lnetcdff -lnetcdf"
+export ESMF_NETCDF_LIBPATH=/app/COST/netcdf-c/4.3.3.1/gnu//lib
+export ESMF_NETCDF_INCLUDE=/app/COST/netcdf-c/4.3.3.1/gnu/include
+export ESMF_NETCDFF_INCLUDE="/app/COST/netcdf-fortran/4.4.2/gnu/include /app/COST/netcdf-c/4.3.3.1/gnu//include /app/COST/hdf5/1.8.15/gnu//include"
+export ESMF_NETCDFF_LIBPATH="/app/COST/netcdf-fortran/4.4.2/gnu/lib /app/COST/netcdf-c/4.3.3.1/gnu//lib /app/COST/hdf5/1.8.15/gnu//lib"
+export ESMF_DIR=/p/work1/mpotts/gfortran_7.3.0_mpt_O
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpt
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 48 clean 2>&1|tee clean_$JOBID.log 
+make -j 48 2>&1|tee build_$JOBID.log
+
