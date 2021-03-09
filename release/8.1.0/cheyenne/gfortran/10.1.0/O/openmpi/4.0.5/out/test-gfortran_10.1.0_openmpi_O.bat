232504cea0984e1ff2c50596b58055b544649b4c
Mark Potts <mark.potts@noaa.gov>
2021-03-09 02:44:46 -0700

    update for test gfortran_10.1.0_openmpi_O on cheyenne [ci skip]

---
+++ release/8.1.0/cheyenne/gfortran/10.1.0/O/openmpi/4.0.5/out/test-gfortran_10.1.0_openmpi_O.bat
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+#PBS -N test-gfortran_10.1.0_openmpi_O.bat
+#PBS -j oe
+#PBS -q regular
+#PBS -A p48503002
+#PBS -l select=1:ncpus=36:mpiprocs=36
+#PBS -l walltime=1:00:00
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /glade/scratch/mpotts/gfortran_10.1.0_openmpi_O
+set -x
+module load gnu/10.1.0 openmpi/4.0.5 netcdf/4.7.4
+module list >& module-test.log
+
+export ESMF_NETCDF=nc-config
+
+export ESMF_F90COMPILEOPTS="-fallow-argument-mismatch -fallow-invalid-boz"
+export ESMF_DIR=/glade/scratch/mpotts/gfortran_10.1.0_openmpi_O
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=openmpi
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make install 2>&1|tee install_$JOBID.log 
+make all_tests 2>&1|tee test_$JOBID.log 
+
---
+++ release/8.1.0/cheyenne/gfortran/9.1.0/O/mpt/2.22/out/test-gfortran_9.1.0_mpt_O.bat
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+#PBS -N test-gfortran_9.1.0_mpt_O.bat
+#PBS -j oe
+#PBS -q regular
+#PBS -A p48503002
+#PBS -l select=1:ncpus=36:mpiprocs=36
+#PBS -l walltime=1:00:00
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /glade/scratch/mpotts/gfortran_9.1.0_mpt_O
+set -x
+module load gnu/9.1.0 mpt/2.22 netcdf/4.7.3
+module list >& module-test.log
+
+export ESMF_NETCDF=nc-config
+
+export ESMF_F90COMPILER=mpif90
+export ESMF_DIR=/glade/scratch/mpotts/gfortran_9.1.0_mpt_O
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpt
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make install 2>&1|tee install_$JOBID.log 
+make all_tests 2>&1|tee test_$JOBID.log 
+
