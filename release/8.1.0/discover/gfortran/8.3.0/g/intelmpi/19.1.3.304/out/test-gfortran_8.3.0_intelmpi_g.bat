c607f06704103b3a06b4ba5041a45c94930aed4c
Mark.Potts <Mark.Potts@noaa.gov>
2021-03-09 01:11:23 -0500

    update for test gfortran_8.3.0_intelmpi_g on discover [ci skip]

---
+++ release/8.1.0/discover/gfortran/8.3.0/g/intelmpi/19.1.3.304/out/test-gfortran_8.3.0_intelmpi_g.bat
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+#SBATCH --account=s2326
+#SBATCH --partition=compute
+#SBATCH --qos=allnccs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=28
+#SBATCH --time=1:00:00
+#SBATCH --exclusive
+#SBATCH --output test-gfortran_8.3.0_intelmpi_g.bat_%j.o
+export JOBID=$SLURM_JOBID
+set -x
+module load comp/gcc/8.3.0 mpi/impi/19.1.3.304 
+module list >& module-test.log
+module list >& module-test.log
+
+export -n ESMF_NETCDF
+
+export ESMF_DIR=/gpfsm/dnb04/projects/p98/mpotts/esmf/gfortran_8.3.0_intelmpi_g
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=intelmpi
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make install 2>&1|tee install_$JOBID.log 
+make all_tests 2>&1|tee test_$JOBID.log 
+
