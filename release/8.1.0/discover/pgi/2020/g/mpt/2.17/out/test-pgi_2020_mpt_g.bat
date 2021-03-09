ba08a2e0c4b483c7d32dcfddd5f9dd12fcb59260
Mark.Potts <Mark.Potts@noaa.gov>
2021-03-09 02:04:27 -0500

    update for test gfortran_10.1.0_intelmpi_g on discover [ci skip]

---
+++ release/8.1.0/discover/pgi/2020/g/mpt/2.17/out/test-pgi_2020_mpt_g.bat
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+#SBATCH --account=s2326
+#SBATCH --partition=compute
+#SBATCH --qos=allnccs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=28
+#SBATCH --time=1:00:00
+#SBATCH --exclusive
+#SBATCH --output test-pgi_2020_mpt_g.bat_%j.o
+export JOBID=$SLURM_JOBID
+set -x
+module load comp/pgi/20.4 mpi/sgi-mpt/2.17 
+module list >& module-test.log
+module list >& module-test.log
+
+export -n ESMF_NETCDF
+
+export ESMF_DIR=/gpfsm/dnb04/projects/p98/mpotts/esmf/pgi_2020_mpt_g
+export ESMF_COMPILER=pgi
+export ESMF_COMM=mpt
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make install 2>&1|tee install_$JOBID.log 
+make all_tests 2>&1|tee test_$JOBID.log 
+
