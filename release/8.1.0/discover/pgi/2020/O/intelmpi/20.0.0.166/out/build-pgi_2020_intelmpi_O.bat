2d2b79c4ce9b15613d27b2104340c81fdd583d8c
Mark.Potts <Mark.Potts@noaa.gov>
2021-03-09 01:55:50 -0500

    update for test intel_2020_mpt_g on discover [ci skip]

---
+++ release/8.1.0/discover/pgi/2020/O/intelmpi/20.0.0.166/out/build-pgi_2020_intelmpi_O.bat
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+#SBATCH --account=s2326
+#SBATCH --partition=compute
+#SBATCH --qos=allnccs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=28
+#SBATCH --time=1:00:00
+#SBATCH --exclusive
+#SBATCH --output build-pgi_2020_intelmpi_O.bat_%j.o
+export JOBID=$SLURM_JOBID
+set -x
+module load comp/pgi/20.4 mpi/impi/20.0.0.166 
+module list >& module-build.log
+module list >& module-build.log
+
+export -n ESMF_NETCDF
+
+export ESMF_DIR=/gpfsm/dnb04/projects/p98/mpotts/esmf/pgi_2020_intelmpi_O
+export ESMF_COMPILER=pgi
+export ESMF_COMM=intelmpi
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 28 clean 2>&1|tee clean_$JOBID.log 
+make -j 28 2>&1|tee build_$JOBID.log
+
