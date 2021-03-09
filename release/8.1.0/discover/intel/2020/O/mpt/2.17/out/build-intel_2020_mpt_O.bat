87bd12a7c6a492a1cc69c9cfe5d57ce626a4efba
Mark.Potts <Mark.Potts@noaa.gov>
2021-03-09 01:14:28 -0500

    update for test intel_2020_mpt_O on discover [ci skip]

---
+++ release/8.1.0/discover/intel/2020/O/mpt/2.17/out/build-intel_2020_mpt_O.bat
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+#SBATCH --account=s2326
+#SBATCH --partition=compute
+#SBATCH --qos=allnccs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=28
+#SBATCH --time=1:00:00
+#SBATCH --exclusive
+#SBATCH --output build-intel_2020_mpt_O.bat_%j.o
+export JOBID=$SLURM_JOBID
+set -x
+export ESMF_F90COMPILER=ifort
+export ESMF_CXXCOMPILER=icpc
+export MPICXX_CXX=icpc
+module load comp/intel/19.1.3.304 mpi/sgi-mpt/2.17 netcdf4/4.7.4
+module load hdf5/1.13.0 
+module list
+module list >& module-build.log
+
+export ESMF_NETCDF=nc-config
+
+export ESMF_NETCDF_LIBS="-lnetcdff -lnetcdf -lhdf5_hl -lhdf5"
+export ESMF_DIR=/gpfsm/dnb04/projects/p98/mpotts/esmf/intel_2020_mpt_O
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpt
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 28 clean 2>&1|tee clean_$JOBID.log 
+make -j 28 2>&1|tee build_$JOBID.log
+
