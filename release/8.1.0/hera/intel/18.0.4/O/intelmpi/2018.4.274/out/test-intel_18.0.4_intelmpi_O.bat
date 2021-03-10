9460821cd54fb8386a08809c1360af9dd4442dd2
Mark A Potts <Mark.Potts@noaa.gov>
2021-03-10 06:03:35 +0000

    update for test intel_18.0.4_intelmpi_O on hera [ci skip]

--- release/8.1.0/hera/intel/18.0.4/O/intelmpi/2018.4.274/out/test-intel_18.0.4_intelmpi_O.bat
+++ release/8.1.0/hera/intel/18.0.4/O/intelmpi/2018.4.274/out/test-intel_18.0.4_intelmpi_O.bat
@@ -8,10 +8,10 @@
 #SBATCH --exclusive
 #SBATCH --output test-intel_18.0.4_intelmpi_O.bat_%j.o
 export JOBID=$SLURM_JOBID
-set -x
 module load intel/18.0.5.274 impi/2018.4.274 netcdf/4.7.0
 module list >& module-test.log
 
+set -x
 export ESMF_NETCDF=nc-config
 
 export ESMF_DIR=/scratch1/NCEPDEV/da/Mark.Potts/sandbox/intel_18.0.4_intelmpi_O
