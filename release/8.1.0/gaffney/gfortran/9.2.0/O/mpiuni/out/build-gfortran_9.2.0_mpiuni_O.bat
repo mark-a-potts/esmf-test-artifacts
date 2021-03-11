e51608fbaf3c713161eff22ef6d4987a86f12a24
Mark Allen Potts <mpotts@gaffney06.ib0.icexa.navydsrc.hpc.mil>
2021-03-11 09:09:21 +0000

    update for build gfortran_9.2.0_mpiuni_O on gaffney [ci skip]

---
+++ release/8.1.0/gaffney/gfortran/9.2.0/O/mpiuni/out/build-gfortran_9.2.0_mpiuni_O.bat
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+#PBS -N build-gfortran_9.2.0_mpiuni_O.bat
+#PBS -j oe
+#PBS -q standard
+#PBS -A NRLMR03795024
+#PBS -l select=1:ncpus=48:mpiprocs=48
+#PBS -l walltime=1:00:00
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /p/work1/mpotts/gfortran_9.2.0_mpiuni_O
+
+module load cseinit
+export ESMF_MPIRUN=/p/work1/mpotts/gfortran_9.2.0_mpiuni_O/src/Infrastructure/stubs/mpiuni/mpirun
+module load gcc/9.2.0  
+
+module list >& module-build.log
+
+set -x
+
+export ESMF_DIR=/p/work1/mpotts/gfortran_9.2.0_mpiuni_O
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 48 clean 2>&1|tee clean_$JOBID.log 
+make -j 48 2>&1|tee build_$JOBID.log
+
