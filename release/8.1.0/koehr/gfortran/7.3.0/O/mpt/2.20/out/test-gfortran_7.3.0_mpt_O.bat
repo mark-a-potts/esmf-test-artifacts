d2f17eb9b421fd5ce65bea1b72e5b444cb052290
Mark Allen Potts <mpotts@koehr01.ib0.icexa.navydsrc.hpc.mil>
2021-03-11 17:11:54 +0000

    update for build gfortran_7.3.0_mpt_g on koehr [ci skip]

--- release/8.1.0/koehr/gfortran/7.3.0/O/mpt/2.20/out/test-gfortran_7.3.0_mpt_O.bat
+++ release/8.1.0/koehr/gfortran/7.3.0/O/mpt/2.20/out/test-gfortran_7.3.0_mpt_O.bat
@@ -33,5 +33,5 @@ export ESMF_TESTWITHTHREADS='ON'
 make install 2>&1|tee install_$JOBID.log 
 make all_tests 2>&1|tee test_$JOBID.log 
 
-./getres-test.sh
+ssh koehr01 /p/work1/mpotts/gfortran_7.3.0_mpt_O/getres-test.sh
 
