733d1a1dc5dbeb5ff2701f5494a1508708f6cda7
Mark Potts <mark.potts@noaa.gov>
2021-03-15 02:44:18 -0600

    update for build gfortran_10.1.0_openmpi_g on cheyenne [ci skip]

--- release/8.1.0/cheyenne/gfortran/10.1.0/g/openmpi/4.0.5/out/test-gfortran_10.1.0_openmpi_g.bat
+++ release/8.1.0/cheyenne/gfortran/10.1.0/g/openmpi/4.0.5/out/test-gfortran_10.1.0_openmpi_g.bat
@@ -8,13 +8,14 @@
 JOBID="`echo $PBS_JOBID | cut -d. -f1`"
 
 cd /glade/scratch/mpotts/gfortran_10.1.0_openmpi_g
-set -x
 module load gnu/10.1.0 openmpi/4.0.5 netcdf/4.7.4
 module list >& module-test.log
 
+set -x
 export ESMF_NETCDF=nc-config
 
 export ESMF_F90COMPILEOPTS="-fallow-argument-mismatch -fallow-invalid-boz"
+export ESMF_F90COMPILER=mpif90
 export ESMF_DIR=/glade/scratch/mpotts/gfortran_10.1.0_openmpi_g
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=openmpi
@@ -23,4 +24,10 @@ export ESMF_TESTEXHAUSTIVE='ON'
 export ESMF_TESTWITHTHREADS='ON'
 make install 2>&1|tee install_$JOBID.log 
 make all_tests 2>&1|tee test_$JOBID.log 
+
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd nuopc-app-prototypes
+./testProtos.sh 2>&1|tee ../nuopc_$JOBID.log 
+
+ssh cheyenne6 /glade/scratch/mpotts/gfortran_10.1.0_openmpi_g/getres-test.sh
 
