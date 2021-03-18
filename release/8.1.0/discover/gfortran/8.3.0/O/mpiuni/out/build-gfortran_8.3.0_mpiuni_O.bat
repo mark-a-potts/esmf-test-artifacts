6bbb3024f294d24eecfcbed0437a0ff55e03cabd
Mark Potts <Mark.Potts@noaa.gov>
2021-03-18 07:49:00 -0400

    restoring missing directories

---
+++ release/8.1.0/discover/gfortran/8.3.0/O/mpiuni/out/build-gfortran_8.3.0_mpiuni_O.bat
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+#SBATCH --account=s2326
+#SBATCH --partition=compute
+#SBATCH --qos=allnccs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=28
+#SBATCH --time=1:00:00
+#SBATCH --exclusive
+#SBATCH --output build-gfortran_8.3.0_mpiuni_O.bat_%j.o
+export JOBID=$SLURM_JOBID
+export ESMF_MPIRUN=/gpfsm/dnb04/projects/p98/mpotts/esmf/gfortran_8.3.0_mpiuni_O/src/Infrastructure/stubs/mpiuni/mpirun
+module load comp/gcc/8.3.0  
+
+module list >& module-build.log
+
+set -x
+
+export ESMF_DIR=/gpfsm/dnb04/projects/p98/mpotts/esmf/gfortran_8.3.0_mpiuni_O
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 28 clean 2>&1|tee clean_$JOBID.log 
+make -j 28 2>&1|tee build_$JOBID.log
+
