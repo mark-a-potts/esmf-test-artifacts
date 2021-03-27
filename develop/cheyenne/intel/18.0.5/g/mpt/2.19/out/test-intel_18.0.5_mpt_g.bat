30db6ce39aba5d0295338e3c697af72603874b61
Mark Potts <mark.potts@noaa.gov>
2021-03-27 09:21:59 -0600

    update for test intel_18.0.5_mpt_g on cheyenne [ci skip]

---
+++ develop/cheyenne/intel/18.0.5/g/mpt/2.19/out/test-intel_18.0.5_mpt_g.bat
@@ -0,0 +1,32 @@
+#!/bin/bash -l
+#PBS -N test-intel_18.0.5_mpt_g.bat
+#PBS -j oe
+#PBS -q regular
+#PBS -A p48503002
+#PBS -l select=1:ncpus=36:mpiprocs=36
+#PBS -l walltime=1:00:00
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /glade/scratch/mpotts/intel_18.0.5_mpt_g
+module load intel/18.0.5 mpt/2.19 netcdf/4.6.3
+module list >& module-test.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/glade/scratch/mpotts/intel_18.0.5_mpt_g
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpt
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make info 2>&1| tee info.log 
+make install 2>&1|tee install_$JOBID.log 
+make all_tests 2>&1|tee test_$JOBID.log 
+
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd nuopc-app-prototypes
+./testProtos.sh 2>&1|tee ../nuopc_$JOBID.log 
+
+ssh cheyenne6 /glade/scratch/mpotts/intel_18.0.5_mpt_g/getres-test.sh
+
