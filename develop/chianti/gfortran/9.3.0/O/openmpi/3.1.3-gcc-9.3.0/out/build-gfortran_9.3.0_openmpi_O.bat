ee67c37a191bdc94f5c3cb7bca68d940616299f3
Mark Potts <Mark.Potts@noaa.gov>
2021-04-08 17:15:35 -0400

    update for build gfortran_9.3.0_openmpi_O on chianti [ci skip]

--- develop/chianti/gfortran/9.3.0/O/openmpi/3.1.3-gcc-9.3.0/out/build-gfortran_9.3.0_openmpi_O.bat
+++ develop/chianti/gfortran/9.3.0/O/openmpi/3.1.3-gcc-9.3.0/out/build-gfortran_9.3.0_openmpi_O.bat
@@ -1,20 +1,19 @@
 #!/bin/bash -l
 export JOBID=$1
-module load None NoModule 
 
+module use /home/mpotts/spack/share/spack/modules/linux-linuxmint19-skylake
+module load gcc/9.3.0-gcc-7.5.0 openmpi/3.1.3-gcc-9.3.0 netcdf-c/4.7.4-gcc-9.3.0-openmpi
+module load hdf5/1.10.7-gcc-9.3.0-openmpi 
+module list
+module load netcdf-fortran/4.5.3-gcc-9.3.0-openmpi 
+module list
 module list >& module-build.log
 
 set -x
+export ESMF_NETCDF=nc-config
 
-export ESMF_NETCDF=/home/mpotts/spack/opt/spack/linux-linuxmint19-skylake/gcc-9.3.0/netcdf-c-4.7.4-mh42cmw5oougqeytguqxjkkrrd2bfdsl/bin/nc-config
-export ESMF_NFCONFIG=/home/mpotts/spack/opt/spack/linux-linuxmint19-skylake/gcc-9.3.0/netcdf-fortran-4.5.3-7cigsuiobyasv6mbext7pmabw5fhzo6y/bin/nf-config
-export CC=/home/mpotts/spack/opt/spack/linux-linuxmint19-skylake/gcc-7.5.0/gcc-9.3.0-3ltccu6cenbq/bin/gcc
-export CXX=/home/mpotts/spack/opt/spack/linux-linuxmint19-skylake/gcc-7.5.0/gcc-9.3.0-3ltccu6cenbq/bin/g++
-export FC=/home/mpotts/spack/opt/spack/linux-linuxmint19-skylake/gcc-7.5.0/gcc-9.3.0-3ltccu6cenbq/bin/gfortran
+export ESMF_NETCDF=nc-config
 export ESMF_NFCONFIG=nf-config
-export PATH=/home/mpotts/spack/opt/spack/linux-linuxmint19-skylake/gcc-9.3.0/openmpi-3.1.6-jnhkylwskoutgnisw5k4agyzw77xr35b/bin:$PATH
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/mpotts/spack/opt/spack/linux-linuxmint19-skylake/gcc-9.3.0/openmpi-3.1.6-jnhkylwskoutgnisw5k4agyzw77xr35b/lib
-export LIBRARY_PATH=$LIBRARY_PATH:/home/mpotts/spack/opt/spack/linux-linuxmint19-skylake/gcc-9.3.0/openmpi-3.1.6-jnhkylwskoutgnisw5k4agyzw77xr35b/lib
 export ESMF_DIR=/home/mpotts/gfortran_9.3.0_openmpi_O
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=openmpi
--- develop/chianti/gfortran/9.3.0/O/openmpi/3.1.3-gcc-9.3.0/out/test-gfortran_9.3.0_openmpi_O.bat
+++ develop/chianti/gfortran/9.3.0/O/openmpi/3.1.3-gcc-9.3.0/out/test-gfortran_9.3.0_openmpi_O.bat
@@ -1,20 +1,19 @@
 #!/bin/bash -l
 export JOBID=$1
-module load None NoModule 
 
+module use /home/mpotts/spack/share/spack/modules/linux-linuxmint19-skylake
+module load gcc/9.3.0-gcc-7.5.0 openmpi/3.1.3-gcc-9.3.0 netcdf-c/4.7.4-gcc-9.3.0-openmpi
+module load hdf5/1.10.7-gcc-9.3.0-openmpi 
+module list
+module load netcdf-fortran/4.5.3-gcc-9.3.0-openmpi 
+module list
 module list >& module-test.log
 
 set -x
+export ESMF_NETCDF=nc-config
 
-export ESMF_NETCDF=/home/mpotts/spack/opt/spack/linux-linuxmint19-skylake/gcc-9.3.0/netcdf-c-4.7.4-mh42cmw5oougqeytguqxjkkrrd2bfdsl/bin/nc-config
-export ESMF_NFCONFIG=/home/mpotts/spack/opt/spack/linux-linuxmint19-skylake/gcc-9.3.0/netcdf-fortran-4.5.3-7cigsuiobyasv6mbext7pmabw5fhzo6y/bin/nf-config
-export CC=/home/mpotts/spack/opt/spack/linux-linuxmint19-skylake/gcc-7.5.0/gcc-9.3.0-3ltccu6cenbq/bin/gcc
-export CXX=/home/mpotts/spack/opt/spack/linux-linuxmint19-skylake/gcc-7.5.0/gcc-9.3.0-3ltccu6cenbq/bin/g++
-export FC=/home/mpotts/spack/opt/spack/linux-linuxmint19-skylake/gcc-7.5.0/gcc-9.3.0-3ltccu6cenbq/bin/gfortran
+export ESMF_NETCDF=nc-config
 export ESMF_NFCONFIG=nf-config
-export PATH=/home/mpotts/spack/opt/spack/linux-linuxmint19-skylake/gcc-9.3.0/openmpi-3.1.6-jnhkylwskoutgnisw5k4agyzw77xr35b/bin:$PATH
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/mpotts/spack/opt/spack/linux-linuxmint19-skylake/gcc-9.3.0/openmpi-3.1.6-jnhkylwskoutgnisw5k4agyzw77xr35b/lib
-export LIBRARY_PATH=$LIBRARY_PATH:/home/mpotts/spack/opt/spack/linux-linuxmint19-skylake/gcc-9.3.0/openmpi-3.1.6-jnhkylwskoutgnisw5k4agyzw77xr35b/lib
 export ESMF_DIR=/home/mpotts/gfortran_9.3.0_openmpi_O
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=openmpi
@@ -29,3 +28,11 @@ export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
 cd nuopc-app-prototypes
 ./testProtos.sh 2>&1|tee ../nuopc_$JOBID.log 
 
+
+export PATH=$PATH:$HOME/.local/bin
+ssh chianti "export PATH=$PATH:$HOME/.local/bin;module load python/3.6.8;cd $PWD; python3 setup.py test_examples_dryrun"
+ssh chianti "export PATH=$PATH:$HOME/.local/bin;module load python/3.6.8;cd $PWD; python3 setup.py test_regrid_from_file_dryrun"
+ssh chianti "export PATH=$PATH:$HOME/.local/bin;module load python/3.6.8;cd $PWD; python3 setup.py test_regrid_from_file_dryrun"
+python3 setup.py test 2>&1 tee python_test.log
+python3 setup.py test_examples 2>&1 tee python_examples.log
+python3 setup.py test_regrid_from_file 2>&1 tee python_regrid.log
