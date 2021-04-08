121f208afa4dca8e4050a8c98e6206f5866cea3b
Mark Potts <mark.potts@noaa.gov>
2021-04-08 15:11:08 -0600

    update for build gfortran_7.4.0_openmpi_O on cheyenne [ci skip]

--- develop/cheyenne/gfortran/7.4.0/O/openmpi/4.0.3/out/test-gfortran_7.4.0_openmpi_O.bat
+++ develop/cheyenne/gfortran/7.4.0/O/openmpi/4.0.3/out/test-gfortran_7.4.0_openmpi_O.bat
@@ -29,5 +29,13 @@ export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
 cd nuopc-app-prototypes
 ./testProtos.sh 2>&1|tee ../nuopc_$JOBID.log 
 
+
+export PATH=$PATH:$HOME/.local/bin
+ssh cheyenne6 "export PATH=$PATH:$HOME/.local/bin;module load python/3.6.8;cd $PWD; python3 setup.py test_examples_dryrun"
+ssh cheyenne6 "export PATH=$PATH:$HOME/.local/bin;module load python/3.6.8;cd $PWD; python3 setup.py test_regrid_from_file_dryrun"
+ssh cheyenne6 "export PATH=$PATH:$HOME/.local/bin;module load python/3.6.8;cd $PWD; python3 setup.py test_regrid_from_file_dryrun"
+python3 setup.py test 2>&1 tee python_test.log
+python3 setup.py test_examples 2>&1 tee python_examples.log
+python3 setup.py test_regrid_from_file 2>&1 tee python_regrid.log
 ssh cheyenne6 /glade/scratch/mpotts/gfortran_7.4.0_openmpi_O/getres-test.sh
 
