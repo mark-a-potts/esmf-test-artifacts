211bef501f15f8436c9bddc9e52d78a957f65ecd
Mark Potts <mark.potts@noaa.gov>
2021-04-08 15:56:25 -0600

    update for build gfortran_7.4.0_openmpi_g on cheyenne [ci skip]

--- develop/cheyenne/gfortran/7.4.0/g/openmpi/4.0.3/out/build-gfortran_7.4.0_openmpi_g.bat
+++ develop/cheyenne/gfortran/7.4.0/g/openmpi/4.0.3/out/build-gfortran_7.4.0_openmpi_g.bat
@@ -21,8 +21,8 @@ export ESMF_COMM=openmpi
 export ESMF_BOPT='g'
 export ESMF_TESTEXHAUSTIVE='ON'
 export ESMF_TESTWITHTHREADS='ON'
-make -j 36 clean 2>&1|tee clean_$JOBID.log 
-make -j 36 2>&1|tee build_$JOBID.log
+make -j 36 clean 2>&1| tee clean_$JOBID.log 
+make -j 36 2>&1| tee build_$JOBID.log
 
 ssh cheyenne6 /glade/scratch/mpotts/gfortran_7.4.0_openmpi_g/getres-build.sh
 
