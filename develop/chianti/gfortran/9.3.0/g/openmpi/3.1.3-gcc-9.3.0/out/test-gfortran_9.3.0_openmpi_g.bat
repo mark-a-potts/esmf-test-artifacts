3bcb4e121f0c7cdc902bd008541e34fe0dbc93c3
Mark Potts <Mark.Potts@noaa.gov>
2021-04-08 17:27:04 -0400

    update for build gfortran_9.3.0_openmpi_g on chianti [ci skip]

--- develop/chianti/gfortran/9.3.0/g/openmpi/3.1.3-gcc-9.3.0/out/test-gfortran_9.3.0_openmpi_g.bat
+++ develop/chianti/gfortran/9.3.0/g/openmpi/3.1.3-gcc-9.3.0/out/test-gfortran_9.3.0_openmpi_g.bat
@@ -28,3 +28,11 @@ export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
 cd nuopc-app-prototypes
 ./testProtos.sh 2>&1|tee ../nuopc_$JOBID.log 
 
+
+export PATH=$PATH:$HOME/.local/bin
+ssh chianti "export PATH=$PATH:$HOME/.local/bin;module load python/3.6.8;cd $PWD; python3 setup.py test_examples_dryrun"
+ssh chianti "export PATH=$PATH:$HOME/.local/bin;module load python/3.6.8;cd $PWD; python3 setup.py test_regrid_from_file_dryrun"
+ssh chianti "export PATH=$PATH:$HOME/.local/bin;module load python/3.6.8;cd $PWD; python3 setup.py test_regrid_from_file_dryrun"
+python3 setup.py test 2>&1 tee python_test.log
+python3 setup.py test_examples 2>&1 tee python_examples.log
+python3 setup.py test_regrid_from_file 2>&1 tee python_regrid.log
