2a511bc91a38c95f59a50d90e02bee5c3af9079a
Mark Allen Potts <mpotts@koehr03.ib0.icexa.navydsrc.hpc.mil>
2021-04-09 06:52:49 +0000

    update for build gfortran_7.3.0_mpt_O on koehr [ci skip]

--- develop/koehr/gfortran/7.3.0/O/mpt/2.20/out/build-gfortran_7.3.0_mpt_O.bat
+++ develop/koehr/gfortran/7.3.0/O/mpt/2.20/out/build-gfortran_7.3.0_mpt_O.bat
@@ -30,8 +30,8 @@ export ESMF_COMM=mpt
 export ESMF_BOPT='O'
 export ESMF_TESTEXHAUSTIVE='ON'
 export ESMF_TESTWITHTHREADS='ON'
-make -j 48 clean 2>&1|tee clean_$JOBID.log 
-make -j 48 2>&1|tee build_$JOBID.log
+make -j 48 clean 2>&1| tee clean_$JOBID.log 
+make -j 48 2>&1| tee build_$JOBID.log
 
 ssh koehr03 /p/work1/mpotts/gfortran_7.3.0_mpt_O/getres-build.sh
 
