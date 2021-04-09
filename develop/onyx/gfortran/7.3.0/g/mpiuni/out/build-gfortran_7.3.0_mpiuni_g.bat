7d9299f5322f9f787b4272cd134851ca4f0dae26
Mark Allen Potts <mpotts@onyx.erdc.hpc.mil>
2021-04-09 16:55:46 -0500

    update for build gfortran_7.3.0_mpiuni_g on onyx [ci skip]

---
+++ develop/onyx/gfortran/7.3.0/g/mpiuni/out/build-gfortran_7.3.0_mpiuni_g.bat
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+#PBS -N build-gfortran_7.3.0_mpiuni_g.bat
+#PBS -j oe
+#PBS -q standard
+#PBS -A NRLMR03795024
+#PBS -l select=1:ncpus=44:mpiprocs=44
+#PBS -l walltime=1:00:00
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /p/work/mpotts/gfortran_7.3.0_mpiuni_g
+
+module unload PrgEnv-cray PrgEnv-intel
+
+module load PrgEnv-gnu
+module load gcc/7.3.0  
+
+module list >& module-build.log
+
+set -x
+
+export ESMF_DIR=/p/work/mpotts/gfortran_7.3.0_mpiuni_g
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 44 clean 2>&1| tee clean_$JOBID.log 
+make -j 44 2>&1| tee build_$JOBID.log
+
+ssh onyx08 /p/work/mpotts/gfortran_7.3.0_mpiuni_g/getres-build.sh
+
