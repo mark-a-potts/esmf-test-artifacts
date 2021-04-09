ab80422ebb6b21357341738a73091cc7b65b4fd3
Mark Allen Potts <mpotts@gaffney06.ib0.icexa.navydsrc.hpc.mil>
2021-04-09 06:26:43 +0000

    update for build intel_2018.1_mpt_O on gaffney [ci skip]

--- develop/gaffney/intel/2018.1/O/mpt/2.20/out/test-intel_2018.1_mpt_O.bat
+++ develop/gaffney/intel/2018.1/O/mpt/2.20/out/test-intel_2018.1_mpt_O.bat
@@ -26,12 +26,12 @@ export ESMF_BOPT='O'
 export ESMF_TESTEXHAUSTIVE='ON'
 export ESMF_TESTWITHTHREADS='ON'
 make info 2>&1| tee info.log 
-make install 2>&1|tee install_$JOBID.log 
-make all_tests 2>&1|tee test_$JOBID.log 
+make install 2>&1| tee install_$JOBID.log 
+make all_tests 2>&1| tee test_$JOBID.log 
 
 export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
 cd nuopc-app-prototypes
-./testProtos.sh 2>&1|tee ../nuopc_$JOBID.log 
+./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
 
 ssh gaffney06 /p/work1/mpotts/intel_2018.1_mpt_O/getres-test.sh
 
