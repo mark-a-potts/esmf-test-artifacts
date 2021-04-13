1c50e5062a34b95da255035eb2fa29632619c9a0
Mark Potts <mpotts@izumi.unified.ucar.edu>
2021-04-13 14:45:53 -0600

    update for build intel_19.1.0.166_mpiuni_g on izumi [ci skip]

---
+++ develop/izumi/intel/19.1.0.166/g/mpiuni/out/build-intel_19.1.0.166_mpiuni_g.bat
@@ -0,0 +1,27 @@
+#!/bin/bash -l
+#PBS -N build-intel_19.1.0.166_mpiuni_g.bat
+#PBS -j oe
+#PBS -q medium
+#PBS -A P93300606
+#PBS -l select=1:ncpus=15:mpiprocs=15
+#PBS -l walltime=1:00:00
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /scratch/cluster/mpotts/intel_19.1.0.166_mpiuni_g
+module load compiler/intel/20.0.1  tool/netcdf/4.7.4/intel/20.0.1
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/scratch/cluster/mpotts/intel_19.1.0.166_mpiuni_g
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 15 clean 2>&1| tee clean_$JOBID.log 
+make -j 15 2>&1| tee build_$JOBID.log
+
+ssh izumi.unified.ucar.edu /scratch/cluster/mpotts/intel_19.1.0.166_mpiuni_g/getres-build.sh
+
