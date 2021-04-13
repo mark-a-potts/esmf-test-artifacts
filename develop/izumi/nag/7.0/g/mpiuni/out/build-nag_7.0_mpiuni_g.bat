a00334b5bda7ad008868a73223e24a0f6fe1ee05
Mark Potts <mpotts@izumi.unified.ucar.edu>
2021-04-13 14:45:57 -0600

    update for build nag_7.0_mpiuni_g on izumi [ci skip]

---
+++ develop/izumi/nag/7.0/g/mpiuni/out/build-nag_7.0_mpiuni_g.bat
@@ -0,0 +1,27 @@
+#!/bin/bash -l
+#PBS -N build-nag_7.0_mpiuni_g.bat
+#PBS -j oe
+#PBS -q medium
+#PBS -A P93300606
+#PBS -l select=1:ncpus=15:mpiprocs=15
+#PBS -l walltime=1:00:00
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /scratch/cluster/mpotts/nag_7.0_mpiuni_g
+module load compiler/nag/7.0  tool/netcdf/c4.7.4-f4.5.2/nag-gnu/7.0-9.3.0
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/scratch/cluster/mpotts/nag_7.0_mpiuni_g
+export ESMF_COMPILER=nag
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 15 clean 2>&1| tee clean_$JOBID.log 
+make -j 15 2>&1| tee build_$JOBID.log
+
+ssh izumi.unified.ucar.edu /scratch/cluster/mpotts/nag_7.0_mpiuni_g/getres-build.sh
+
