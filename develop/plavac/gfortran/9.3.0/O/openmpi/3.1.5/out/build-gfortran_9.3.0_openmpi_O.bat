a86d9b6eb03a5ffad6ffac02ad394dd75f5f60b8
Mark A Potts <mark.potts@noaa.gov>
2021-04-13 14:23:34 -0400

    update for build gfortran_9.3.0_openmpi_O on plavac [ci skip]

--- develop/plavac/gfortran/9.3.0/O/openmpi/3.1.5/out/build-gfortran_9.3.0_openmpi_O.bat
+++ develop/plavac/gfortran/9.3.0/O/openmpi/3.1.5/out/build-gfortran_9.3.0_openmpi_O.bat
@@ -1,6 +1,8 @@
 #!/usr/local/bin/bash -l
 export JOBID=$1
 module load gcc 3.1.5 netcdf-c/4.7.3
+module load hdf5/1.10.6 
+module list
 module load netcdf-fortran/4.5.2 
 module list
 module list >& module-build.log
@@ -10,14 +12,8 @@ export ESMF_NETCDF=nc-config
 
 export ESMF_NETCDF=nc-config
 export ESMF_NFCONFIG=nf-config
-export CC=/usr/local/bin/gcc-9
-export CXX=/usr/local/bin/g++-9
-export FC=/usr/local/bin/gfortran-9
-export ESMF_CXXLINKPATHS="-L$HDF5_ROOT/lib $HDF5LDFLAGS -lz"
-export ESMF_F90COMPILER=$FC
+export ESMF_F90COMPILER=mpif90
 export ESMF_F90LINKER=mpif90
-export ESMF_F90LINKPATHS="-L$HDF5_ROOT/lib $HDF5LDFLAGS -lz"
-export ESMF_NETCDF_LIBS="-lnetcdff -lnetcdf -lhdf5_hl -lhdf5 $HDF5ExtraLibs -lz"
 export ESMF_DIR=/Users/mpotts/gfortran_9.3.0_openmpi_O
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=openmpi
