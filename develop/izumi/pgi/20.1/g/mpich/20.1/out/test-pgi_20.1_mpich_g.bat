5af401026b86431f3a2c8c4965bc29cc363cee0f
Mark Potts <mpotts@izumi.unified.ucar.edu>
2021-04-13 16:25:44 -0600

    update for build pgi_20.1_mpich_g on izumi [ci skip]

---
+++ develop/izumi/pgi/20.1/g/mpich/20.1/out/test-pgi_20.1_mpich_g.bat
@@ -0,0 +1,32 @@
+#!/bin/bash -l
+#PBS -N test-pgi_20.1_mpich_g.bat
+#PBS -j oe
+#PBS -q medium
+#PBS -A P93300606
+#PBS -l select=1:ncpus=15:mpiprocs=15
+#PBS -l walltime=1:00:00
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+cd /scratch/cluster/mpotts/pgi_20.1_mpich_g
+module load compiler/pgi/20.1 mpi/2.3.3/pgi/20.1 tool/netcdf/4.7.4/pgi/20.1
+module list >& module-test.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/scratch/cluster/mpotts/pgi_20.1_mpich_g
+export ESMF_COMPILER=pgi
+export ESMF_COMM=mpich
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make info 2>&1| tee info.log 
+make install 2>&1| tee install_$JOBID.log 
+make all_tests 2>&1| tee test_$JOBID.log 
+
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
+
+ssh izumi.unified.ucar.edu /scratch/cluster/mpotts/pgi_20.1_mpich_g/getres-test.sh
+
