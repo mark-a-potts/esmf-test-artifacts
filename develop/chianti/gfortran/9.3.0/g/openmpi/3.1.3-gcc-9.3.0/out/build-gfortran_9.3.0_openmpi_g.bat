cd0206ce34a66421777e5091b3563da995ebb985
Mark Potts <Mark.Potts@noaa.gov>
2021-04-14 15:45:06 -0400

    Update from chianti

--- develop/chianti/gfortran/9.3.0/g/openmpi/3.1.3-gcc-9.3.0/out/build-gfortran_9.3.0_openmpi_g.bat
+++ develop/chianti/gfortran/9.3.0/g/openmpi/3.1.3-gcc-9.3.0/out/build-gfortran_9.3.0_openmpi_g.bat
@@ -2,6 +2,8 @@
 export JOBID=$1
 
 module use /home/mpotts/spack/share/spack/modules/linux-linuxmint19-skylake
+
+module load python
 module load gcc/9.3.0-gcc-7.5.0 openmpi/3.1.3-gcc-9.3.0 netcdf-c/4.7.4-gcc-9.3.0-openmpi
 module load hdf5/1.10.7-gcc-9.3.0-openmpi 
 module list
