dce8a839ab1a9ee179a66ee799390fdb28601d1d
Mark Potts <mark.potts@noaa.gov>
2021-04-15 06:46:12 -0600

    update for build gfortran_10.1.0_mpt_O_develop on cheyenne [ci skip]

--- develop/cheyenne/gfortran/10.1.0/O/mpt/2.23/out/test-gfortran_10.1.0_mpt_O.bat
+++ develop/cheyenne/gfortran/10.1.0/O/mpt/2.23/out/test-gfortran_10.1.0_mpt_O.bat
@@ -7,7 +7,7 @@
 #PBS -l walltime=1:00:00
 JOBID="`echo $PBS_JOBID | cut -d. -f1`"
 
-cd /glade/scratch/mpotts/gfortran_10.1.0_mpt_O
+cd /glade/scratch/mpotts/gfortran_10.1.0_mpt_O_develop
 module load gnu/10.1.0 mpt/2.23 netcdf/4.7.4
 module list >& module-test.log
 
@@ -16,7 +16,7 @@ export ESMF_NETCDF=nc-config
 
 export ESMF_F90COMPILEOPTS="-fallow-argument-mismatch -fallow-invalid-boz"
 export ESMF_F90COMPILER=mpif90
-export ESMF_DIR=/glade/scratch/mpotts/gfortran_10.1.0_mpt_O
+export ESMF_DIR=/glade/scratch/mpotts/gfortran_10.1.0_mpt_O_develop
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=mpt
 export ESMF_BOPT='O'
@@ -30,5 +30,5 @@ export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
 cd nuopc-app-prototypes
 ./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
 
-ssh cheyenne6 /glade/scratch/mpotts/gfortran_10.1.0_mpt_O/getres-test.sh
+ssh cheyenne6 /glade/scratch/mpotts/gfortran_10.1.0_mpt_O_develop/getres-test.sh
 
