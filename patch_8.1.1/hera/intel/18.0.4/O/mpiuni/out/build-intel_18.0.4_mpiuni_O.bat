b375cc1ef63b943f5430c11e9127e725ec1eb5ec
Mark A Potts <Mark.Potts@noaa.gov>
2021-04-15 21:59:54 +0000

    update for build intel_18.0.4_mpiuni_O_patch_8.1.1 on hera [ci skip]

---
+++ patch_8.1.1/hera/intel/18.0.4/O/mpiuni/out/build-intel_18.0.4_mpiuni_O.bat
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+#SBATCH --account=da-cpu
+#SBATCH --partition=hera
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --time=1:00:00
+#SBATCH --exclusive
+#SBATCH --output build-intel_18.0.4_mpiuni_O.bat_%j.o
+export JOBID=$SLURM_JOBID
+export ESMF_MPIRUN=/scratch1/NCEPDEV/da/Mark.Potts/sandbox/intel_18.0.4_mpiuni_O_patch_8.1.1/src/Infrastructure/stubs/mpiuni/mpirun
+module load intel/18.0.5.274  netcdf/4.7.0
+module list >& module-build.log
+
+set -x
+export ESMF_NETCDF=nc-config
+
+export ESMF_DIR=/scratch1/NCEPDEV/da/Mark.Potts/sandbox/intel_18.0.4_mpiuni_O_patch_8.1.1
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+make -j 40 clean 2>&1| tee clean_$JOBID.log 
+make -j 40 2>&1| tee build_$JOBID.log
+
