61df2ecb157899c3d86786b785230972dba11c51
Mark Potts <mpotts@redlineperf.com>
2021-04-15 08:37:48 -0500

    update for build gfortran_8.3.0_mpiuni_g_develop on orion [ci skip]

--- develop/orion/gfortran/8.3.0/g/mpiuni/out/build-gfortran_8.3.0_mpiuni_g.bat
+++ develop/orion/gfortran/8.3.0/g/mpiuni/out/build-gfortran_8.3.0_mpiuni_g.bat
@@ -15,7 +15,7 @@ set -x
 export ESMF_NETCDF=nc-config
 
 export LD_PRELOAD=/apps/gcc-8/gcc-8.3.0/lib64/libstdc++.so
-export ESMF_DIR=/work/noaa/da/mpotts/sandbox/gfortran_8.3.0_mpiuni_g
+export ESMF_DIR=/work/noaa/da/mpotts/sandbox/gfortran_8.3.0_mpiuni_g_develop
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=mpiuni
 export ESMF_BOPT='g'
