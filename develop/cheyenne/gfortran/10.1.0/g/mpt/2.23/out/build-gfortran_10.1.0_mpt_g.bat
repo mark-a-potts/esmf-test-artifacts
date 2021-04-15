4f63341f3f931601affb5b6396634181b9ce1fab
Mark Potts <mark.potts@noaa.gov>
2021-04-15 06:50:03 -0600

    update for build gfortran_10.1.0_mpt_g_develop on cheyenne [ci skip]

--- develop/cheyenne/gfortran/10.1.0/g/mpt/2.23/out/build-gfortran_10.1.0_mpt_g.bat
+++ develop/cheyenne/gfortran/10.1.0/g/mpt/2.23/out/build-gfortran_10.1.0_mpt_g.bat
@@ -7,7 +7,7 @@
 #PBS -l walltime=1:00:00
 JOBID="`echo $PBS_JOBID | cut -d. -f1`"
 
-cd /glade/scratch/mpotts/gfortran_10.1.0_mpt_g
+cd /glade/scratch/mpotts/gfortran_10.1.0_mpt_g_develop
 module load gnu/10.1.0 mpt/2.23 netcdf/4.7.4
 module list >& module-build.log
 
@@ -16,7 +16,7 @@ export ESMF_NETCDF=nc-config
 
 export ESMF_F90COMPILEOPTS="-fallow-argument-mismatch -fallow-invalid-boz"
 export ESMF_F90COMPILER=mpif90
-export ESMF_DIR=/glade/scratch/mpotts/gfortran_10.1.0_mpt_g
+export ESMF_DIR=/glade/scratch/mpotts/gfortran_10.1.0_mpt_g_develop
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=mpt
 export ESMF_BOPT='g'
@@ -25,5 +25,5 @@ export ESMF_TESTWITHTHREADS='ON'
 make -j 36 clean 2>&1| tee clean_$JOBID.log 
 make -j 36 2>&1| tee build_$JOBID.log
 
-ssh cheyenne6 /glade/scratch/mpotts/gfortran_10.1.0_mpt_g/getres-build.sh
+ssh cheyenne6 /glade/scratch/mpotts/gfortran_10.1.0_mpt_g_develop/getres-build.sh
 
