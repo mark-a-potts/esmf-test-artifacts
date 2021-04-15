d1911e1cc9f61a9396e048c5053849edfc1b85cb
Mark Potts <mpotts@cgdm-catania.cgd.ucar.edu>
2021-04-15 07:04:06 -0600

    update for build gfortran_9.3.0_mpiuni_O_develop on catania [ci skip]

--- develop/catania/gfortran/9.3.0/O/mpiuni/out/build-gfortran_9.3.0_mpiuni_O.bat
+++ develop/catania/gfortran/9.3.0/O/mpiuni/out/build-gfortran_9.3.0_mpiuni_O.bat
@@ -2,7 +2,7 @@
 export JOBID=$1
 
 module use /project/esmf/stack/modulefiles
-export ESMF_MPIRUN=/Volumes/esmf/mpotts/gfortran_9.3.0_mpiuni_O/src/Infrastructure/stubs/mpiuni/mpirun
+export ESMF_MPIRUN=/Volumes/esmf/mpotts/gfortran_9.3.0_mpiuni_O_develop/src/Infrastructure/stubs/mpiuni/mpirun
 module load gnu/9.3.0/compiler  gnu/9.3.0/netcdf-c/4.7.4
 module load gnu/9.3.0/netcdf-fortran/4.5.3 
 module list
@@ -11,7 +11,7 @@ module list >& module-build.log
 set -x
 export ESMF_NETCDF=nc-config
 
-export ESMF_DIR=/Volumes/esmf/mpotts/gfortran_9.3.0_mpiuni_O
+export ESMF_DIR=/Volumes/esmf/mpotts/gfortran_9.3.0_mpiuni_O_develop
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=mpiuni
 export ESMF_BOPT='O'
