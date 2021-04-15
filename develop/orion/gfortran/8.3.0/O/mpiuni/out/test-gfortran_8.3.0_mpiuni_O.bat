67d5430804627b0d10165e0a25a36d96cbcfddd8
Mark Potts <mpotts@redlineperf.com>
2021-04-15 08:34:00 -0500

    update for build gfortran_8.3.0_mpiuni_O_develop on orion [ci skip]

--- develop/orion/gfortran/8.3.0/O/mpiuni/out/test-gfortran_8.3.0_mpiuni_O.bat
+++ develop/orion/gfortran/8.3.0/O/mpiuni/out/test-gfortran_8.3.0_mpiuni_O.bat
@@ -8,7 +8,7 @@
 #SBATCH --exclusive
 #SBATCH --output test-gfortran_8.3.0_mpiuni_O.bat_%j.o
 export JOBID=$SLURM_JOBID
-export ESMF_MPIRUN=/work/noaa/da/mpotts/sandbox/gfortran_8.3.0_mpiuni_O/src/Infrastructure/stubs/mpiuni/mpirun
+export ESMF_MPIRUN=/work/noaa/da/mpotts/sandbox/gfortran_8.3.0_mpiuni_O_develop/src/Infrastructure/stubs/mpiuni/mpirun
 module load gcc/8.3.0  netcdf/4.7.2
 module list >& module-test.log
 
@@ -16,7 +16,7 @@ set -x
 export ESMF_NETCDF=nc-config
 
 export LD_PRELOAD=/apps/gcc-8/gcc-8.3.0/lib64/libstdc++.so
-export ESMF_DIR=/work/noaa/da/mpotts/sandbox/gfortran_8.3.0_mpiuni_O
+export ESMF_DIR=/work/noaa/da/mpotts/sandbox/gfortran_8.3.0_mpiuni_O_develop
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=mpiuni
 export ESMF_BOPT='O'
