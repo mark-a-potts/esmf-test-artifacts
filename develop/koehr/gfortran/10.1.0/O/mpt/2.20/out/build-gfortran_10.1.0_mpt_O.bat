d7c742afaa57ecee72cb02af47e090386f297253
Mark Potts <Mark.Potts@noaa.gov>
2021-04-15 09:00:24 -0400

    Update from koehr

--- develop/koehr/gfortran/10.1.0/O/mpt/2.20/out/build-gfortran_10.1.0_mpt_O.bat
+++ develop/koehr/gfortran/10.1.0/O/mpt/2.20/out/build-gfortran_10.1.0_mpt_O.bat
@@ -7,7 +7,7 @@
 #PBS -l walltime=1:00:00
 JOBID="`echo $PBS_JOBID | cut -d. -f1`"
 
-cd /p/work1/mpotts/gfortran_10.1.0_mpt_O
+cd /p/work1/mpotts/gfortran_10.1.0_mpt_O_develop
 
 module unload compiler/intel mpt
 module load gcc/10.1.0 mpt/2.20 
@@ -17,7 +17,7 @@ module list >& module-build.log
 set -x
 
 export ESMF_F90COMPILEOPTS="-fallow-argument-mismatch -fallow-invalid-boz"
-export ESMF_DIR=/p/work1/mpotts/gfortran_10.1.0_mpt_O
+export ESMF_DIR=/p/work1/mpotts/gfortran_10.1.0_mpt_O_develop
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=mpt
 export ESMF_BOPT='O'
@@ -26,5 +26,5 @@ export ESMF_TESTWITHTHREADS='ON'
 make -j 48 clean 2>&1| tee clean_$JOBID.log 
 make -j 48 2>&1| tee build_$JOBID.log
 
-ssh koehr06 /p/work1/mpotts/gfortran_10.1.0_mpt_O/getres-build.sh
+ssh koehr06 /p/work1/mpotts/gfortran_10.1.0_mpt_O_develop/getres-build.sh
 
