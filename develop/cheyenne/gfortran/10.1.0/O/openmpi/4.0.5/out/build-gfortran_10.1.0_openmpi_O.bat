dcd4f723c354ab8c55306ce8ec52830f7809cf55
Mark Potts <Mark.Potts@noaa.gov>
2021-04-15 09:00:13 -0400

    Update from cheyenne

--- develop/cheyenne/gfortran/10.1.0/O/openmpi/4.0.5/out/build-gfortran_10.1.0_openmpi_O.bat
+++ develop/cheyenne/gfortran/10.1.0/O/openmpi/4.0.5/out/build-gfortran_10.1.0_openmpi_O.bat
@@ -7,7 +7,7 @@
 #PBS -l walltime=1:00:00
 JOBID="`echo $PBS_JOBID | cut -d. -f1`"
 
-cd /glade/scratch/mpotts/gfortran_10.1.0_openmpi_O
+cd /glade/scratch/mpotts/gfortran_10.1.0_openmpi_O_develop
 module load gnu/10.1.0 openmpi/4.0.5 netcdf/4.7.4
 module list >& module-build.log
 
@@ -16,7 +16,7 @@ export ESMF_NETCDF=nc-config
 
 export ESMF_F90COMPILEOPTS="-fallow-argument-mismatch -fallow-invalid-boz"
 export ESMF_F90COMPILER=mpif90
-export ESMF_DIR=/glade/scratch/mpotts/gfortran_10.1.0_openmpi_O
+export ESMF_DIR=/glade/scratch/mpotts/gfortran_10.1.0_openmpi_O_develop
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=openmpi
 export ESMF_BOPT='O'
@@ -25,5 +25,5 @@ export ESMF_TESTWITHTHREADS='ON'
 make -j 36 clean 2>&1| tee clean_$JOBID.log 
 make -j 36 2>&1| tee build_$JOBID.log
 
-ssh cheyenne6 /glade/scratch/mpotts/gfortran_10.1.0_openmpi_O/getres-build.sh
+ssh cheyenne6 /glade/scratch/mpotts/gfortran_10.1.0_openmpi_O_develop/getres-build.sh
 
--- develop/cheyenne/gfortran/9.1.0/O/openmpi/4.0.5/out/test-gfortran_9.1.0_openmpi_O.bat
+++ develop/cheyenne/gfortran/9.1.0/O/openmpi/4.0.5/out/test-gfortran_9.1.0_openmpi_O.bat
@@ -7,7 +7,7 @@
 #PBS -l walltime=1:00:00
 JOBID="`echo $PBS_JOBID | cut -d. -f1`"
 
-cd /glade/scratch/mpotts/gfortran_9.1.0_openmpi_O
+cd /glade/scratch/mpotts/gfortran_9.1.0_openmpi_O_develop
 module load gnu/9.1.0 openmpi/4.0.5 netcdf/4.7.3
 module list >& module-test.log
 
@@ -15,7 +15,7 @@ set -x
 export ESMF_NETCDF=nc-config
 
 export ESMF_F90COMPILER=mpif90
-export ESMF_DIR=/glade/scratch/mpotts/gfortran_9.1.0_openmpi_O
+export ESMF_DIR=/glade/scratch/mpotts/gfortran_9.1.0_openmpi_O_develop
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=openmpi
 export ESMF_BOPT='O'
@@ -29,5 +29,5 @@ export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
 cd nuopc-app-prototypes
 ./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
 
-ssh cheyenne6 /glade/scratch/mpotts/gfortran_9.1.0_openmpi_O/getres-test.sh
+ssh cheyenne6 /glade/scratch/mpotts/gfortran_9.1.0_openmpi_O_develop/getres-test.sh
 
