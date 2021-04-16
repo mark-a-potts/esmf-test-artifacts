b751dba084306e7b998824ce7864c810148d1bba
Mark A Potts <Mark.Potts@noaa.gov>
2021-04-16 09:45:48 -0400

    update for build gfortran_8.3.0_mpiuni_O_develop on gaea [ci skip]

--- develop/gaea/gfortran/8.3.0/O/mpi/7.7.11/out/test-gfortran_8.3.0_mpi_O.bat
+++ develop/gaea/gfortran/8.3.0/O/mpi/7.7.11/out/test-gfortran_8.3.0_mpi_O.bat
@@ -1,25 +1,23 @@
-#!/bin/bash -l
+#!/bin/sh -l
 #SBATCH --account=nggps_emc
+#SBATCH -o test-gfortran_8.3.0_mpi_O.bat_%j.o
+#SBATCH -e test-gfortran_8.3.0_mpi_O.bat_%j.e
+#SBATCH --time=1:00:00
 #SBATCH --cluster=c4
 #SBATCH --qos=normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH --time=2:00:00
 #SBATCH --exclusive
-#SBATCH --output test-gfortran_8.3.0_mpi_O.bat_%j.o
-export JOBID=$SLURM_JOBID
 
 module unload PrgEnv-intel
 
 module load PrgEnv-gnu
 module load gcc/8.3.0 cray-mpich/7.7.11 cray-netcdf/4.6.3.2
-module list >& module-test.log
-
 set -x
 export ESMF_NETCDF=nc-config
 
 export ESMF_NETCDF_LIBS="-lnetcdff -lnetcdf"
-export ESMF_DIR=/lustre/f2/dev/ncep/Mark.Potts/gfortran_8.3.0_mpi_O
+export ESMF_DIR=/lustre/f2/dev/ncep/Mark.Potts/gfortran_8.3.0_mpi_O_develop
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=mpi
 export ESMF_BOPT='O'
@@ -28,8 +26,4 @@ export ESMF_TESTWITHTHREADS='ON'
 make info 2>&1| tee info.log 
 make install 2>&1| tee install_$JOBID.log 
 make all_tests 2>&1| tee test_$JOBID.log 
-
-export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
-cd nuopc-app-prototypes
-./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
-
+chmod +x runpython.sh
