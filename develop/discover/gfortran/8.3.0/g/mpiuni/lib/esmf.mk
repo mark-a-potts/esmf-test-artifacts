520a249e005b560492d4e5c33f830323bed8d2fe
Mark Potts <Mark.Potts@noaa.gov>
2021-04-20 02:45:08 -0400

    Update from discover

--- develop/discover/gfortran/8.3.0/g/mpiuni/lib/esmf.mk
+++ develop/discover/gfortran/8.3.0/g/mpiuni/lib/esmf.mk
@@ -1,3 +1,4 @@
+build time -- 2021-04-20 01:25:19.455207
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/discover/intel/2020/O/intelmpi/19.1.3.304/lib/esmf.mk
+++ develop/discover/intel/2020/O/intelmpi/19.1.3.304/lib/esmf.mk
@@ -1,3 +1,4 @@
+build time -- 2021-04-20 01:21:39.780750
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
