f703a71a9d7a2a20216657aafd6d581b5dc8ce23
Mark A Potts <Mark.Potts@noaa.gov>
2021-04-20 19:56:25 +0000

    update for build intel_18.0.4_intelmpi_g_patch_8.1.1 on hera [ci skip]

--- patch_8.1.1/hera/intel/18.0.4/g/intelmpi/2018.4.274/out/test-intel_18.0.4_intelmpi_g.bat
+++ patch_8.1.1/hera/intel/18.0.4/g/intelmpi/2018.4.274/out/test-intel_18.0.4_intelmpi_g.bat
@@ -1,12 +1,13 @@
-#!/bin/bash -l
+#!/bin/sh -l
 #SBATCH --account=da-cpu
+#SBATCH -o test-intel_18.0.4_intelmpi_g.bat_%j.o
+#SBATCH -e test-intel_18.0.4_intelmpi_g.bat_%j.e
+#SBATCH --time=1:30:00
 #SBATCH --partition=hera
 #SBATCH --qos=batch
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=40
-#SBATCH --time=1:00:00
 #SBATCH --exclusive
-#SBATCH --output test-intel_18.0.4_intelmpi_g.bat_%j.o
 export JOBID=$SLURM_JOBID
 module load intel/18.0.5.274 impi/2018.4.274 netcdf/4.7.0
 module list >& module-test.log
@@ -23,19 +24,17 @@ export ESMF_TESTWITHTHREADS='ON'
 make info 2>&1| tee info.log 
 make install 2>&1| tee install_$JOBID.log 
 make all_tests 2>&1| tee test_$JOBID.log 
-
 export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
 cd nuopc-app-prototypes
 ./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
 
+chmod +x runpython.sh
 
 cd ../src/addon/ESMPy
 
 export PATH=$PATH:$HOME/.local/bin
 python3 setup.py build 2>&1 | tee python_build.log
-ssh hfe02 "export PATH=$PATH:$HOME/.local/bin;module load python/3.6.8;cd $PWD; python3 setup.py test_examples_dryrun"
-ssh hfe02 "export PATH=$PATH:$HOME/.local/bin;module load python/3.6.8;cd $PWD; python3 setup.py test_regrid_from_file_dryrun"
-ssh hfe02 "export PATH=$PATH:$HOME/.local/bin;module load python/3.6.8;cd $PWD; python3 setup.py test_regrid_from_file_dryrun"
+ssh hfe02 /scratch1/NCEPDEV/da/Mark.Potts/sandbox/intel_18.0.4_intelmpi_g_patch_8.1.1/runpython.sh 2>&1 | tee python_build.log
 python3 setup.py test 2>&1 | tee python_test.log
 python3 setup.py test_examples 2>&1 | tee python_examples.log
 python3 setup.py test_regrid_from_file 2>&1 | tee python_regrid.log
