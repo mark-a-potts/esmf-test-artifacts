92f03eb621b2fafd54f7a4a3de7fae1b16c305c0
Mark Potts <Mark.Potts@noaa.gov>
2021-04-20 13:30:06 -0400

    Update from gaffney

--- develop/gaffney/gfortran/9.2.0/g/mpiuni/out/build-gfortran_9.2.0_mpiuni_g.bat
+++ develop/gaffney/gfortran/9.2.0/g/mpiuni/out/build-gfortran_9.2.0_mpiuni_g.bat
@@ -1,10 +1,10 @@
-#!/bin/bash -l
+#!/bin/sh -l
 #PBS -N build-gfortran_9.2.0_mpiuni_g.bat
-#PBS -j oe
+#PBS -l walltime=1:00:00
+#PBS -l walltime=1:00:00
 #PBS -q standard
 #PBS -A NRLMR03795024
 #PBS -l select=1:ncpus=48:mpiprocs=48
-#PBS -l walltime=1:00:00
 JOBID="`echo $PBS_JOBID | cut -d. -f1`"
 
 cd /p/work1/mpotts/gfortran_9.2.0_mpiuni_g_develop
@@ -24,6 +24,4 @@ export ESMF_TESTEXHAUSTIVE='ON'
 export ESMF_TESTWITHTHREADS='ON'
 make -j 48 clean 2>&1| tee clean_$JOBID.log 
 make -j 48 2>&1| tee build_$JOBID.log
-
-ssh gaffney06 /p/work1/mpotts/gfortran_9.2.0_mpiuni_g_develop/getres-build.sh
 
