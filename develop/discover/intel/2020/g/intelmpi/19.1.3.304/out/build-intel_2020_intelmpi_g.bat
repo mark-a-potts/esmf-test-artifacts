91ab8b27538b71f88cecdd50ca36601b6e0af28e
Mark.Potts <Mark.Potts@noaa.gov>
2021-04-20 15:29:16 -0400

    update for build intel_2020_intelmpi_g_develop on discover [ci skip]

--- develop/discover/intel/2020/g/intelmpi/19.1.3.304/out/build-intel_2020_intelmpi_g.bat
+++ develop/discover/intel/2020/g/intelmpi/19.1.3.304/out/build-intel_2020_intelmpi_g.bat
@@ -1,16 +1,16 @@
-#!/bin/bash -l
+#!/bin/sh -l
 #SBATCH --account=s2326
+#SBATCH -o build-intel_2020_intelmpi_g.bat_%j.o
+#SBATCH -e build-intel_2020_intelmpi_g.bat_%j.e
+#SBATCH --time=1:20:00
 #SBATCH --partition=compute
 #SBATCH --qos=allnccs
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=28
-#SBATCH --time=1:00:00
 #SBATCH --exclusive
-#SBATCH --output build-intel_2020_intelmpi_g.bat_%j.o
 export JOBID=$SLURM_JOBID
 module load comp/intel/19.1.3.304 mpi/impi/19.1.3.304 netcdf4/4.7.4
 module load hdf5/1.13.0 
-module list
 module list >& module-build.log
 
 set -x
