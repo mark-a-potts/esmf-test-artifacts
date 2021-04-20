014f243e042d82c603554c383a7b5e585e4e672c
Mark Potts <mark.potts@noaa.gov>
2021-04-20 12:59:04 -0600

    update for build intel_18.0.5_openmpi_g_develop on cheyenne [ci skip]

--- develop/cheyenne/intel/18.0.5/g/openmpi/3.1.4/out/build-intel_18.0.5_openmpi_g.bat
+++ develop/cheyenne/intel/18.0.5/g/openmpi/3.1.4/out/build-intel_18.0.5_openmpi_g.bat
@@ -1,10 +1,10 @@
-#!/bin/bash -l
+#!/bin/sh -l
 #PBS -N build-intel_18.0.5_openmpi_g.bat
-#PBS -j oe
+#PBS -l walltime=1:00:00
+#PBS -l walltime=1:00:00
 #PBS -q regular
 #PBS -A p48503002
 #PBS -l select=1:ncpus=36:mpiprocs=36
-#PBS -l walltime=1:00:00
 JOBID="`echo $PBS_JOBID | cut -d. -f1`"
 
 cd /glade/scratch/mpotts/intel_18.0.5_openmpi_g_develop
@@ -24,4 +24,3 @@ make -j 36 clean 2>&1| tee clean_$JOBID.log
 make -j 36 2>&1| tee build_$JOBID.log
 
 ssh cheyenne6 /glade/scratch/mpotts/intel_18.0.5_openmpi_g_develop/getres-build.sh
-
