4270cd23677913851a36feb361a8e73f1f308c5f
Mark Potts <Mark.Potts@noaa.gov>
2021-04-20 09:00:17 -0400

    Update from cheyenne

--- develop/cheyenne/intel/18.0.5/O/mpt/2.19/lib/esmf.mk
+++ develop/cheyenne/intel/18.0.5/O/mpt/2.19/lib/esmf.mk
@@ -1,3 +1,4 @@
+build time -- 2021-04-20 06:05:11.720396
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
--- develop/cheyenne/intel/18.0.5/g/intelmpi/2018.4.274/lib/esmf.mk
+++ develop/cheyenne/intel/18.0.5/g/intelmpi/2018.4.274/lib/esmf.mk
@@ -1,3 +1,4 @@
+build time -- 2021-04-20 06:03:07.690621
 # ESMF application makefile fragment
 #
 # Use the following ESMF_ variables to compile and link
