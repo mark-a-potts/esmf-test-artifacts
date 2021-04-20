58affa03d40eaceee6ff2ada318f862b46543f55
Mark Allen Potts <mpotts@gaffney06.ib0.icexa.navydsrc.hpc.mil>
2021-04-20 18:52:25 +0000

    update for build intel_2018.1_intelmpi_O_develop on gaffney [ci skip]

--- develop/gaffney/intel/2018.1/g/intelmpi/18.0.1.163/out/build-intel_2018.1_intelmpi_g.bat
+++ develop/gaffney/intel/2018.1/g/intelmpi/18.0.1.163/out/build-intel_2018.1_intelmpi_g.bat
@@ -1,10 +1,10 @@
-#!/bin/bash -l
+#!/bin/sh -l
 #PBS -N build-intel_2018.1_intelmpi_g.bat
-#PBS -j oe
+#PBS -l walltime=1:00:00
+#PBS -l walltime=1:00:00
 #PBS -q standard
 #PBS -A NRLMR03795024
 #PBS -l select=1:ncpus=48:mpiprocs=48
-#PBS -l walltime=1:00:00
 JOBID="`echo $PBS_JOBID | cut -d. -f1`"
 
 cd /p/work1/mpotts/intel_2018.1_intelmpi_g_develop
@@ -12,7 +12,6 @@ cd /p/work1/mpotts/intel_2018.1_intelmpi_g_develop
 module load cseinit-intel
 module load compiler/intel/18.0.1.163 compiler/intelmpi/18.0.1.163 cse/netcdf/latest
 module load cse/netcdf_fortran/latest 
-module list
 module list >& module-build.log
 
 set -x
@@ -29,4 +28,3 @@ make -j 48 clean 2>&1| tee clean_$JOBID.log
 make -j 48 2>&1| tee build_$JOBID.log
 
 ssh gaffney06 /p/work1/mpotts/intel_2018.1_intelmpi_g_develop/getres-build.sh
-
