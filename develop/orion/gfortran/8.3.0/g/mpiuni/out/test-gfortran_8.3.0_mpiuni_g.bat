fdf3497796d227594480a8524e53ddd8deb073c4
Mark Potts <mpotts@redlineperf.com>
2021-04-20 13:54:38 -0500

    update for build gfortran_8.3.0_mpiuni_g_develop on orion [ci skip]

--- develop/orion/gfortran/8.3.0/g/mpiuni/out/test-gfortran_8.3.0_mpiuni_g.bat
+++ develop/orion/gfortran/8.3.0/g/mpiuni/out/test-gfortran_8.3.0_mpiuni_g.bat
@@ -1,12 +1,13 @@
-#!/bin/bash -l
+#!/bin/sh -l
 #SBATCH --account=da-cpu
+#SBATCH -o test-gfortran_8.3.0_mpiuni_g.bat_%j.o
+#SBATCH -e test-gfortran_8.3.0_mpiuni_g.bat_%j.e
+#SBATCH --time=1:00:00
 #SBATCH --partition=orion
 #SBATCH --qos=batch
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=40
-#SBATCH --time=1:00:00
 #SBATCH --exclusive
-#SBATCH --output test-gfortran_8.3.0_mpiuni_g.bat_%j.o
 export JOBID=$SLURM_JOBID
 module load gcc/8.3.0  netcdf/4.7.2
 module list >& module-test.log
@@ -24,8 +25,3 @@ export ESMF_TESTWITHTHREADS='ON'
 make info 2>&1| tee info.log 
 make install 2>&1| tee install_$JOBID.log 
 make all_tests 2>&1| tee test_$JOBID.log 
-
-export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
-cd nuopc-app-prototypes
-./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
-
