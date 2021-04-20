5f98f394ad23b7d088fb101b8da1756c1933dc2c
Mark Potts <Mark.Potts@noaa.gov>
2021-04-20 16:30:08 -0400

    Update from catania

--- develop/catania/gfortran/10.2.0/g/mpich3/3.3.2-custom/out/test-gfortran_10.2.0_mpich3_g.bat
+++ develop/catania/gfortran/10.2.0/g/mpich3/3.3.2-custom/out/test-gfortran_10.2.0_mpich3_g.bat
@@ -1,10 +1,9 @@
 #!/bin/bash -l
-export JOBID=$1
+export JOBID=12346
 
 module use /project/esmf/stack/modulefiles
 module load gnu/10.2.0/compiler gnu/10.2.0/mpich3/3.3.2-custom gnu/10.2.0/netcdf-c/4.7.4
 module load gnu/10.2.0/netcdf-fortran/4.5.3 
-module list
 module list >& module-test.log
 
 set -x
@@ -20,8 +19,3 @@ export ESMF_TESTWITHTHREADS='ON'
 make info 2>&1| tee info.log 
 make install 2>&1| tee install_$JOBID.log 
 make all_tests 2>&1| tee test_$JOBID.log 
-
-export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
-cd nuopc-app-prototypes
-./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
-
