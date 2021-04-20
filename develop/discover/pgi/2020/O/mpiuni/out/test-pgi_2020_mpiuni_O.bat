9d5b6d52ae4240c740bf7494ad4e4556738a3aa7
Mark.Potts <Mark.Potts@noaa.gov>
2021-04-20 15:49:45 -0400

    update for build pgi_2020_mpiuni_O_develop on discover [ci skip]

--- develop/discover/pgi/2020/O/mpiuni/out/test-pgi_2020_mpiuni_O.bat
+++ develop/discover/pgi/2020/O/mpiuni/out/test-pgi_2020_mpiuni_O.bat
@@ -1,14 +1,14 @@
-#!/bin/bash -l
+#!/bin/sh -l
 #SBATCH --account=s2326
+#SBATCH -o test-pgi_2020_mpiuni_O.bat_%j.o
+#SBATCH -e test-pgi_2020_mpiuni_O.bat_%j.e
+#SBATCH --time=1:20:00
 #SBATCH --partition=compute
 #SBATCH --qos=allnccs
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=28
-#SBATCH --time=1:20:00
 #SBATCH --exclusive
-#SBATCH --output test-pgi_2020_mpiuni_O.bat_%j.o
 export JOBID=$SLURM_JOBID
-export ESMF_MPIRUN=/gpfsm/dnb04/projects/p98/mpotts/esmf/pgi_2020_mpiuni_O_develop/src/Infrastructure/stubs/mpiuni/mpirun
 module load comp/pgi/20.4  
 
 module list >& module-test.log
@@ -24,8 +24,3 @@ export ESMF_TESTWITHTHREADS='ON'
 make info 2>&1| tee info.log 
 make install 2>&1| tee install_$JOBID.log 
 make all_tests 2>&1| tee test_$JOBID.log 
-
-export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
-cd nuopc-app-prototypes
-./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
-
