0c5f2817b00a178e7202b52167bd929f2b19bad4
Mark Allen Potts <mpotts@gaffney06.ib0.icexa.navydsrc.hpc.mil>
2021-04-20 18:49:36 +0000

    update for build gfortran_9.2.0_mpiuni_O_develop on gaffney [ci skip]

--- develop/gaffney/gfortran/9.2.0/g/mpiuni/out/test-gfortran_9.2.0_mpiuni_g.bat
+++ develop/gaffney/gfortran/9.2.0/g/mpiuni/out/test-gfortran_9.2.0_mpiuni_g.bat
@@ -24,3 +24,4 @@ export ESMF_TESTWITHTHREADS='ON'
 make info 2>&1| tee info.log 
 make install 2>&1| tee install_$JOBID.log 
 make all_tests 2>&1| tee test_$JOBID.log 
+ssh gaffney06 /p/work1/mpotts/gfortran_9.2.0_mpiuni_g_develop/getres-test.sh
