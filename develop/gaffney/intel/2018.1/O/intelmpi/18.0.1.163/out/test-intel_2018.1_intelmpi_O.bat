22a507e03c77662942f36e034f36048a191e2ccf
Mark Allen Potts <mpotts@gaffney06.ib0.icexa.navydsrc.hpc.mil>
2021-04-20 12:48:24 +0000

    update for build intel_2018.1_intelmpi_O_develop on gaffney [ci skip]

--- develop/gaffney/intel/2018.1/O/intelmpi/18.0.1.163/out/test-intel_2018.1_intelmpi_O.bat
+++ develop/gaffney/intel/2018.1/O/intelmpi/18.0.1.163/out/test-intel_2018.1_intelmpi_O.bat
@@ -7,7 +7,7 @@
 #PBS -l walltime=1:00:00
 JOBID="`echo $PBS_JOBID | cut -d. -f1`"
 
-cd /p/work1/mpotts/intel_2018.1_intelmpi_O
+cd /p/work1/mpotts/intel_2018.1_intelmpi_O_develop
 
 module load cseinit-intel
 module load compiler/intel/18.0.1.163 compiler/intelmpi/18.0.1.163 cse/netcdf/latest
@@ -19,7 +19,7 @@ set -x
 export ESMF_NETCDF=nc-config
 
 export ESMF_F90COMPILEOPTS=-I$CSE_NETCDF_FORTRAN_INCLUDE_DIR
-export ESMF_DIR=/p/work1/mpotts/intel_2018.1_intelmpi_O
+export ESMF_DIR=/p/work1/mpotts/intel_2018.1_intelmpi_O_develop
 export ESMF_COMPILER=intel
 export ESMF_COMM=intelmpi
 export ESMF_BOPT='O'
@@ -33,5 +33,5 @@ export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
 cd nuopc-app-prototypes
 ./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
 
-ssh gaffney06 /p/work1/mpotts/intel_2018.1_intelmpi_O/getres-test.sh
+ssh gaffney06 /p/work1/mpotts/intel_2018.1_intelmpi_O_develop/getres-test.sh
 
