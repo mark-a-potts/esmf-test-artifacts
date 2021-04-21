f84585a3ef4271e23e54c39b28e11e6ec622dc49
Mark Allen Potts <mpotts@koehr05.ib0.icexa.navydsrc.hpc.mil>
2021-04-21 14:27:03 +0000

    update for build intel_2019.4_mpt_O_develop on koehr [ci skip]

--- develop/koehr/gfortran/10.1.0/O/mpt/2.20/out/build-gfortran_10.1.0_mpt_O.bat
+++ develop/koehr/gfortran/10.1.0/O/mpt/2.20/out/build-gfortran_10.1.0_mpt_O.bat
@@ -1,10 +1,10 @@
-#!/bin/bash -l
+#!/bin/sh -l
 #PBS -N build-gfortran_10.1.0_mpt_O.bat
-#PBS -j oe
+#PBS -l walltime=1:00:00
+#PBS -l walltime=1:00:00
 #PBS -q standard
 #PBS -A NRLMR03795024
 #PBS -l select=1:ncpus=48:mpiprocs=48
-#PBS -l walltime=1:00:00
 JOBID="`echo $PBS_JOBID | cut -d. -f1`"
 
 cd /p/work1/mpotts/gfortran_10.1.0_mpt_O_develop
@@ -26,5 +26,4 @@ export ESMF_TESTWITHTHREADS='ON'
 make -j 48 clean 2>&1| tee clean_$JOBID.log 
 make -j 48 2>&1| tee build_$JOBID.log
 
-ssh koehr06 /p/work1/mpotts/gfortran_10.1.0_mpt_O_develop/getres-build.sh
-
+ssh koehr05 /p/work1/mpotts/gfortran_10.1.0_mpt_O_develop/getres-build.sh
