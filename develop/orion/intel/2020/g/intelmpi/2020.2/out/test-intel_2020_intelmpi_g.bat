ef757163891a9ff20e65b52cc8b83d983388069f
Mark Potts <Mark.Potts@noaa.gov>
2021-04-21 15:30:12 -0400

    Update from orion

--- develop/orion/intel/2020/g/intelmpi/2020.2/out/test-intel_2020_intelmpi_g.bat
+++ develop/orion/intel/2020/g/intelmpi/2020.2/out/test-intel_2020_intelmpi_g.bat
@@ -25,10 +25,10 @@ make info 2>&1| tee info.log
 make install 2>&1| tee install_$JOBID.log 
 make all_tests 2>&1| tee test_$JOBID.log 
 export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+chmod +x runpython.sh
 cd nuopc-app-prototypes
 ./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
 
-chmod +x runpython.sh
 
 cd ../src/addon/ESMPy
 
