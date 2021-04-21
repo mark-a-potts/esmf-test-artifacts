5b9ab681b6e1f51b784bd8b3714bf9741496410a
Mark A Potts <Mark.Potts@noaa.gov>
2021-04-21 13:17:38 +0000

    update for build gfortran_9.2.0_intelmpi_O_patch_8.1.1 on hera [ci skip]

--- patch_8.1.1/hera/gfortran/9.2.0/O/intelmpi/2020/out/test-gfortran_9.2.0_intelmpi_O.bat
+++ patch_8.1.1/hera/gfortran/9.2.0/O/intelmpi/2020/out/test-gfortran_9.2.0_intelmpi_O.bat
@@ -2,7 +2,7 @@
 #SBATCH --account=da-cpu
 #SBATCH -o test-gfortran_9.2.0_intelmpi_O.bat_%j.o
 #SBATCH -e test-gfortran_9.2.0_intelmpi_O.bat_%j.e
-#SBATCH --time=1:30:00
+#SBATCH --time=2:00:00
 #SBATCH --partition=hera
 #SBATCH --qos=batch
 #SBATCH --nodes=1
@@ -31,16 +31,16 @@ make info 2>&1| tee info.log
 make install 2>&1| tee install_$JOBID.log 
 make all_tests 2>&1| tee test_$JOBID.log 
 export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+chmod +x runpython.sh
 cd nuopc-app-prototypes
 ./testProtos.sh 2>&1| tee ../nuopc_$JOBID.log 
 
-chmod +x runpython.sh
 
 cd ../src/addon/ESMPy
 
 export PATH=$PATH:$HOME/.local/bin
 python3 setup.py build 2>&1 | tee python_build.log
-ssh hfe10 /scratch1/NCEPDEV/da/Mark.Potts/sandbox/gfortran_9.2.0_intelmpi_O_patch_8.1.1/runpython.sh 2>&1 | tee python_build.log
+ssh hfe02 /scratch1/NCEPDEV/da/Mark.Potts/sandbox/gfortran_9.2.0_intelmpi_O_patch_8.1.1/runpython.sh 2>&1 | tee python_build.log
 python3 setup.py test 2>&1 | tee python_test.log
 python3 setup.py test_examples 2>&1 | tee python_examples.log
 python3 setup.py test_regrid_from_file 2>&1 | tee python_regrid.log
