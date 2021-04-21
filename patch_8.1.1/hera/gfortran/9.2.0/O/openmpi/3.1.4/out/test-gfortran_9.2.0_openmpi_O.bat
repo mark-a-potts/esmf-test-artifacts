b6b80e4429902cdad268d95409bbd35983cd6af5
Mark A Potts <Mark.Potts@noaa.gov>
2021-04-21 06:39:53 +0000

    update for build gfortran_9.2.0_intelmpi_O_patch_8.1.1 on hera [ci skip]

--- patch_8.1.1/hera/gfortran/9.2.0/O/openmpi/3.1.4/out/test-gfortran_9.2.0_openmpi_O.bat
+++ patch_8.1.1/hera/gfortran/9.2.0/O/openmpi/3.1.4/out/test-gfortran_9.2.0_openmpi_O.bat
@@ -40,7 +40,7 @@ cd ../src/addon/ESMPy
 
 export PATH=$PATH:$HOME/.local/bin
 python3 setup.py build 2>&1 | tee python_build.log
-ssh hfe02 /scratch1/NCEPDEV/da/Mark.Potts/sandbox/gfortran_9.2.0_openmpi_O_patch_8.1.1/runpython.sh 2>&1 | tee python_build.log
+ssh hfe10 /scratch1/NCEPDEV/da/Mark.Potts/sandbox/gfortran_9.2.0_openmpi_O_patch_8.1.1/runpython.sh 2>&1 | tee python_build.log
 python3 setup.py test 2>&1 | tee python_test.log
 python3 setup.py test_examples 2>&1 | tee python_examples.log
 python3 setup.py test_regrid_from_file 2>&1 | tee python_regrid.log
