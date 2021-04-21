c32723a5f4abc090cadb9217c22835dad790a2ca
Mark Potts <Mark.Potts@noaa.gov>
2021-04-21 14:30:06 -0400

    Update from hera

--- patch_8.1.1/hera/gfortran/9.2.0/g/intelmpi/2020/out/test-gfortran_9.2.0_intelmpi_g.bat
+++ patch_8.1.1/hera/gfortran/9.2.0/g/intelmpi/2020/out/test-gfortran_9.2.0_intelmpi_g.bat
@@ -2,7 +2,7 @@
 #SBATCH --account=da-cpu
 #SBATCH -o test-gfortran_9.2.0_intelmpi_g.bat_%j.o
 #SBATCH -e test-gfortran_9.2.0_intelmpi_g.bat_%j.e
-#SBATCH --time=2:00:00
+#SBATCH --time=1:30:00
 #SBATCH --partition=hera
 #SBATCH --qos=batch
 #SBATCH --nodes=1
