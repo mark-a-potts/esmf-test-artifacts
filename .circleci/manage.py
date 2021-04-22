5c5be0cc433bf5cc96724306e00e9ffe98b0c4bb
Mark A Potts <mark.potts@noaa.gov>
2021-04-22 13:49:12 -0400

    python config

---
+++ .circleci/manage.py
@@ -0,0 +1,14 @@
+import yaml
+import os
+import time
+import subprocess
+import sys
+import pathlib
+
+
+def main(argv):  
+  print("hey! cwd is {}".format(os.getcwd()))
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+  
