c8b5728fc5ba0abf3252a05ebdb5105361006c10
Mark A Potts <Mark.Potts@noaa.gov>
2021-04-22 12:22:41 +0000

    update for build gfortran_9.2.0_mpiuni_O_patch_8.1.1 on hera [ci skip]

--- develop/hera/gfortran/9.2.0/O/openmpi/3.1.4/out/build-gfortran_9.2.0_openmpi_O.bat
+++ develop/hera/gfortran/9.2.0/O/openmpi/3.1.4/out/build-gfortran_9.2.0_openmpi_O.bat
@@ -9,6 +9,7 @@
 #SBATCH --ntasks-per-node=40
 #SBATCH --exclusive
 export JOBID=$SLURM_JOBID
+export ESMF_MPIRUN=mpirun.srun
 module load gnu/9.2.0 openmpi/3.1.4 netcdf/4.7.2
 module load hdf5/1.10.5 
 module list >& module-build.log
@@ -21,7 +22,7 @@ export ESMF_NETCDF_INCLUDE=$NETCDF/include
 export ESMF_NETCDF_LIBPATH=$NETCDF/lib
 export ESMF_NETCDF_LIBS="-lnetcdff -lnetcdf -lhdf5_hl -lhdf5 $HDF5ExtraLibs"
 export ESMF_NETCDF=nc-config
-export ESMF_DIR=/scratch1/NCEPDEV/da/Mark.Potts/sandbox/gfortran_9.2.0_openmpi_O_develop
+export ESMF_DIR=/scratch1/NCEPDEV/stmp2/Mark.Potts/gfortran_9.2.0_openmpi_O_develop
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=openmpi
 export ESMF_BOPT='O'
