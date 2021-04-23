9d14341297ed8bd851c2619324791fd87eff41a3
Mark Potts <Mark.Potts@noaa.gov>
2021-04-23 14:52:05 -0400

    update for test2 of gfortran_9.3.0_openmpi_g_develop with hash ESMF_8_2_0_beta_snapshot_05-2-g3f3d15a on chianti

--- .circleci/runscp.sh
+++ .circleci/runscp.sh
@@ -11,12 +11,13 @@ git branch
 #git branch -r
 export host=`git show -s --format=%s |  awk -F " " '{print $10}'`
 export branch=`git show -s --format=%s |  awk -F " " '{print $5}' | awk -F"_._" '{print $2}'`
-echo $host
-echo $branch
-git checkout main
-git checkout origin/$host $branch/$host
 export hash=`git show -s --format=%s |  awk -F " " '{print $8}'`
 export message=`git show -s --format=%s`
+echo $host
+echo $branch
+git checkout origin/main
+git checkout -b main
+git checkout origin/$host $branch/$host
 echo $message
 find $branch -iname summary.dat | xargs grep -l "hash = $hash" | xargs grep -L "hash = $hash." | xargs grep "test results" | sed 's/\// /g'  | sed -e 's/\t/ /g' | sed -e 's/ \+/ /g' | sed -e 's/mpiuni/mpiuni none/g'  | awk -F " " '{print $2,$3,$4,$6,$7,$5,$12,$14}' > "$branch/$hash.summary"
 git add $branch
